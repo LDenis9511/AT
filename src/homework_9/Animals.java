64007c4e2eeba9a1a30e0c2ff6b2ea71efdaa2da
Denis <lapat9511@gmail.com>
2025-05-21 22:04:33 +0300

    added Homework_9

---
+++ src/homework_9/Animals.java
@@ -0,0 +1,28 @@
+package homework_9;
+
+import java.util.LinkedList;
+
+public class Animals {
+    private LinkedList<String> animals = new LinkedList<>();
+
+    public void setAnimals(String animals) {
+        this.animals.addFirst(animals);
+    }
+
+    public void delAnimals() {
+        this.animals.removeLast();
+    }
+
+    public LinkedList<String> getAnimals() {
+        return animals;
+    }
+}
+
+
+
+
+
+
+
+
+
